a391b8f453a3653f2813960cf643fb5bf60744ac
Minjae Chang <minjaejchang@gmail.com>
2021-11-25 19:49:21 +1300

    Create a form to add new todo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+import { useState, useRef, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [todo, setInput] = useState('');
+  const [todoList, setTodoList] = useState([]);
+
+  const inputRef = useRef(null);
+
+  const handleAdd = (e) => {
+    e.preventDefault();
+    addTodo(todo);
+    setInput('');
+  };
+
+  const addTodo = (todo) => {
+    if (todo === '') {
+      return;
+    }
+
+    const newTodo = {
+      id: Date.now(),
+      name: todo,
+    };
+
+    setTodoList([newTodo, ...todoList]);
+  };
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, [todoList]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {/* form */}
+      <div className='form'>
+        <h1>To Do List</h1>
+        <form>
+          <label>
+            <input
+              ref={inputRef}
+              type='text'
+              onChange={(e) => {
+                setInput(e.target.value);
+              }}
+              value={todo}
+              required
+            />
+          </label>
+          <button onClick={handleAdd}>Add</button>
+        </form>
+      </div>
+
+      {/* list */}
+      <div className='todo'>
+        <ul className='todo-list'>
+          {todoList.map((todo) => (
+            <li key={todo.id} className='todo-item'>
+              <input type='checkbox' />
+              <span>{todo.name}</span>
+              <button className='delete'>delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
