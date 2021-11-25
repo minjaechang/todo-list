56ed57bbe18c99946d122ecbfb90e505c4f82f5b
Minjae Chang <minjaejchang@gmail.com>
2021-11-25 21:11:31 +1300

    Update button and icon

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,13 +1,18 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+
 import './Todo.css';
 
 export default function Todo({ onDelete, todo }) {
   return (
     <li className='todo-item'>
-      <input type='checkbox' />
+      <input className='todo__checkbox' type='checkbox' />
       <span className='todo__title'>{todo.name}</span>
-      <button className='delete' onClick={() => onDelete(todo.id)}>
-        X
-      </button>
+      <FontAwesomeIcon
+        icon={faTrashAlt}
+        className='todo__delete-btn'
+        onClick={() => onDelete(todo.id)}
+      />
     </li>
   );
 }
